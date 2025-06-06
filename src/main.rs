a989388b2c31d8e33d722cf2bffdd3a8ad9085e4
Ramona Łuczkiewicz <ja@agares.info>
2025-06-06 19:08:40 +0200

    the dirties of prototypes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![deny(clippy::all, clippy::pedantic, clippy::nursery, warnings)]
+mod types;
+
+use inkwell::context::Context;
+use types::ByteCode;
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::create();
+    let bytecode = ByteCode::new(&context);
+    let result = bytecode.execute();
+
+    println!("result: {result}");
 }
